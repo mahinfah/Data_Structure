0afb79858726ce7d88af81c4deb0b1acd02f9946
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-07-04 00:23:37 -0800

    Update task4.cpp

--- Structure/Assignment/task4.cpp
+++ Structure/Assignment/task4.cpp
@@ -1,1 +1,49 @@
 // problem question
+#include<iostream>
+using namespace std;
+void show(int a[2][2])
+{
+   cout<<"the matrix are :";
+   cout<<endl;
+ for(int i =0 ;i<2;i++)
+ {
+     //cout<<"the matrix are :";
+     for(int j=0; j<2;j++)
+     {
+         cout<<a[i][j]<<" ";
+     }
+ cout<<endl;
+ }
+}
+
+int sum(int a[2][2],int b[2][2],int c[2][2],int d[2][2])
+{
+     int sum=0;
+
+    cout<<"The new matrix is "<<endl;
+ for(int i =0 ;i<2;i++)
+ {
+     for(int j=0; j<2;j++)
+     {
+            cout<<a[i][j]+b[i][j]+c[i][j]+d[i][j]<<"  ";
+     }
+cout<<endl;
+ }
+
+
+
+}
+
+int main()
+{
+   int a[2][2]={1,1,1,1};
+   int b[2][2]={1,1,1,1};
+   int c[2][2]={1,1,1,1};
+   int d[2][2]={1,1,1,1};
+
+    show(a);
+    show(b);
+    show(c);
+    show(d);
+   sum(a,b,c,d);
+}
