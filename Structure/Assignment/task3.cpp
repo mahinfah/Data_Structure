18bbe691a2ee922215c055ffafb538123ef146b9
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-07-06 14:43:12 +0600

    Update task3.cpp

--- Structure/Assignment/task3.cpp
+++ Structure/Assignment/task3.cpp
@@ -1,1 +1,29 @@
 // question 
+#include <iostream>
+
+using namespace std;
+
+void zero_small(int &a, int &b) {
+  if (a < b) {
+    a = 0;
+  } else {
+    b = 0;
+
+  }
+}
+
+int main() {
+  int a, b;
+
+
+  cout << "Enter two Digits: ";
+  cin >> a >> b;
+
+
+  zero_small(a,b);
+
+
+  cout << "After setting the smaller number zero: " << a << " " << b<< endl;
+
+  return 0;
+}
