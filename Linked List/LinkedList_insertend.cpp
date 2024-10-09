2755dd159c7bced3f10e13a31a931efbdce62772
Kazi Fahad Mahin <151399467+mahinfah@users.noreply.github.com>
2024-10-09 12:23:53 -0800

    Update LinkedList_insertend.cpp

--- Linked List/LinkedList_insertend.cpp
+++ Linked List/LinkedList_insertend.cpp
@@ -1,51 +1,46 @@
 #include<iostream>
 using namespace std;
-class Node{
+
+class Node {
 public:
-int data;
-Node* next;
+    int data;
+    Node* next;
 
-Node(int val)
-{
-    data = val;
-    next = nullptr;
-}
-
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
 };
-int main()
-{
 
-  Node *head;
-  head =nullptr;
-  //creating tail to insert in the end
-  Node *tail = nullptr;
+int main() {
+    Node *head = nullptr;
+    Node *tail = nullptr;
 
-  int a[4]={1,2,3,4};
-int i ;
-  while(i<4)
- {
-  if(head==nullptr)
- {
-    head = new Node(a[i]);
-    tail = head;
- }
- else
-  {
-      Node *temp;
-    temp = new Node(a[i]);
-    temp=temp->next;
-   // tail=tail->next;
+    int a[4] = {1, 2, 3, 4};
+    int i = 0; // Initialize i
 
-  }
-   i++;
- }
+    while (i < 4) {
+        if (head == nullptr) {
+            head = new Node(a[i]);
+            tail = head;
+        } else {
+            Node *temp = new Node(a[i]);
+            tail->next = temp; // Link the new node
+            tail = temp; // Update the tail pointer
+        }
+        i++;
+    }
 
- Node *print = head;
- while(print!=nullptr)
- {
-     cout<<print->data<<"  ";
-     print=print->next;
- }
+    Node *print = head;
+    while (print != nullptr) {
+        cout << print->data << "  ";
+        print = print->next;
+    }
 
-
+    // Free allocated memory
+    while (head != nullptr) {
+        Node *temp = head;
+        head = head->next;
+        delete temp;
+    }
 }
